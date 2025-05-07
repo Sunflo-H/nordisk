7368969f426cb3af4f1058f7c5d36a1cb8f6bd2b
Sunflo-H <sunflo0524@gmail.com>
2025-05-07 23:51:25 +0900

    모달에서 사이즈별 재고 띄우기 성공공

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import ProductCard from './ProductCard';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import ProductCard from "./ProductCard";
 type ProductRow = {
   상품코드: string;
   상품명: string;
   칼라: string;
   수량: number;
-
-  "00": number;
-  "01": number;
-  "02": number;
-  "03": number;
-  "04": number;
-  "05": number;
-  "06": number;
-  "07": number;
-  "08": number;
-  "09": number;
-  "10": number;
-  "11": number;
-  "12": number;
-  "13": number;
-  "14": number;
-  "15": number;
-  "16": number;
-  "17": number;
-  "18": number;
-  "19": number;
+  재고: {
+    "00": number;
+    "01": number;
+    "02": number;
+    "03": number;
+    "04": number;
+    "05": number;
+    "06": number;
+    "07": number;
+    "08": number;
+    "09": number;
+    "10": number;
+    "11": number;
+    "12": number;
+    "13": number;
+    "14": number;
+    "15": number;
+    "16": number;
+    "17": number;
+    "18": number;
+    "19": number;
+  };
 };
-const product:ProductRow = {
+const product: ProductRow = {
   상품코드: "NDK-GRN-001",
   상품명: "Green Puffer Jacket",
-  칼라:"E1",
-  수량:6,
-  "00": 0,
-  "01": 0,
-  "02": 1,
-  "03": 1,
-  "04": 1,
-  "05": 1,
-  "06": 1,
-  "07": 0,
-  "08": 1,
-  "09": 0,
-  "10": 0,
-  "11": 0,
-  "12": 0,
-  "13": 0,
-  "14": 0,
-  "15": 0,
-  "16": 0,
-  "17": 0,
-  "18": 0,
-  "19": 0,
+  칼라: "E1",
+  수량: 6,
+  재고: {
+    "00": 0,
+    "01": 0,
+    "02": 1,
+    "03": 1,
+    "04": 1,
+    "05": 1,
+    "06": 1,
+    "07": 0,
+    "08": 1,
+    "09": 0,
+    "10": 0,
+    "11": 0,
+    "12": 0,
+    "13": 0,
+    "14": 0,
+    "15": 0,
+    "16": 0,
+    "17": 0,
+    "18": 0,
+    "19": 0,
+  },
 };
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      
-     <ProductCard product={product}/>
-      
+      <ProductCard product={product} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
