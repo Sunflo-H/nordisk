ac6f76a45d57d7513f4f05f082e0d2c8bda50cc9
Sunflo-h <sunflo0524@gmail.com>
2025-05-12 20:56:09 +0900

    디자인

--- src/ProductDetailModal.tsx
+++ src/ProductDetailModal.tsx
@@ -1,14 +1,12 @@
 const ProductDetailModal=({product, onClose}) => {
 
     return(
-        <div className="fixed inset-0 z-50 bg-white/50 w-40 border-black-1px">
-            <div className="font-xl">X</div>
-            <div className="flex">
-            <div>
-                -
-            </div>
-            <div>COUNT 1</div>
-            <div>+</div>
+        <div className="fixed inset-0 z-50 bg-white/50  flex flex-col justify-center border w-[90%]">
+            <div className="text-xl">X</div>
+            <div className="flex justify-center gap-4">
+                <div className="bg-red-500 border rounded-sm w-[2%] font-bold">-</div>
+                <div>1</div>
+                <div className="bg-green-500 border rounded-sm w-[2%] font-bold">+</div>
             </div>
         </div>
     );
