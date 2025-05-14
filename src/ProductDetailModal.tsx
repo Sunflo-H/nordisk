21426191276e7cbf1c6e93d55bddd64e167d1633
Sunflo-H <sunflo0524@gmail.com>
2025-05-14 23:37:51 +0900

    디테일 모달 닫기 기능능

--- src/ProductDetailModal.tsx
+++ src/ProductDetailModal.tsx
@@ -1,15 +1,16 @@
-const ProductDetailModal=({product, onClose}) => {
-
-    return(
-        <div className="fixed inset-0 z-50 bg-white/50  flex flex-col justify-center border w-[90%]">
-            <div className="text-xl">X</div>
-            <div className="flex justify-center gap-4">
-                <div className="bg-red-500 border rounded-sm w-[2%] font-bold">-</div>
-                <div>1</div>
-                <div className="bg-green-500 border rounded-sm w-[2%] font-bold">+</div>
-            </div>
-        </div>
-    );
-}
+const ProductDetailModal = ({ product, onClose }) => {
+  return (
+    <div className="fixed inset-0 z-50 bg-white/50  flex flex-col justify-center border w-[90%]">
+      <div className="text-xl" onClick={onClose}>
+        X
+      </div>
+      <div className="flex justify-center gap-4">
+        <div className="bg-red-500 border rounded-sm w-[2%] font-bold">-</div>
+        <div>1</div>
+        <div className="bg-green-500 border rounded-sm w-[2%] font-bold">+</div>
+      </div>
+    </div>
+  );
+};
 
 export default ProductDetailModal;
