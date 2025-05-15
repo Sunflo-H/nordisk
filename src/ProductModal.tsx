2d1b8be2aa5539315238590554211343f5825823
Sunflo-H <sunflo0524@gmail.com>
2025-05-16 00:00:15 +0900

    잔디 제발1

--- src/ProductModal.tsx
+++ src/ProductModal.tsx
@@ -24,7 +24,7 @@ const ProductModal = ({ product, onClose }) => {
 
   const handleIncreae = (num) => {};
 
-  const handleUpdate = () => {};
+  const handleUpdate = (num) => {};
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
