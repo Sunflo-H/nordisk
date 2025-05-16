db254eb3d938e0d6bddd1f55cb9a3f15d85fdace
Sunflo-H <sunflo0524@gmail.com>
2025-05-17 00:01:31 +0900

    타입을 따로 빼서 import로 적용

--- src/ProductCard.tsx
+++ src/ProductCard.tsx
@@ -1,38 +1,7 @@
 import { useState } from "react";
 import ProductModal from "./ProductModal";
-export type SizeKey =
-  | "00"
-  | "01"
-  | "02"
-  | "03"
-  | "04"
-  | "05"
-  | "06"
-  | "07"
-  | "08"
-  | "09"
-  | "10"
-  | "11"
-  | "12"
-  | "13"
-  | "14"
-  | "15"
-  | "16"
-  | "17"
-  | "18"
-  | "19";
+import type { ProductType } from "./types";
 
-export type 재고Type = {
-  [key in SizeKey]: number;
-};
-
-type ProductType = {
-  상품코드: string;
-  상품명: string;
-  칼라: string;
-  수량: number;
-  재고: 재고Type;
-};
 type ProductCardProps = {
   product: ProductType;
 };
