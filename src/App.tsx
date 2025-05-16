30521ca7fabe773bfaa77b07d25e632acf7d1558
Sunflo-H <sunflo0524@gmail.com>
2025-05-16 23:56:36 +0900

    상품 타입 지정

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,38 @@ import ProductCard from "./ProductCard";
 import ExcelReader from "./ExcelReader";
 import { readData } from "./firebase/firebaseDatabase";
 
+export type SizeKey =
+  | "00"
+  | "01"
+  | "02"
+  | "03"
+  | "04"
+  | "05"
+  | "06"
+  | "07"
+  | "08"
+  | "09"
+  | "10"
+  | "11"
+  | "12"
+  | "13"
+  | "14"
+  | "15"
+  | "16"
+  | "17"
+  | "18"
+  | "19";
+
+export type 재고Type = {
+  [key in SizeKey]: number;
+};
+
 type ProductType = {
   상품코드: string;
   상품명: string;
   칼라: string;
   수량: number;
-  재고: {
-    [key: string]: number;
-  };
+  재고: 재고Type;
 };
 
 function App() {
