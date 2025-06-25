38fd13620c398a982636e5cb98ba839459659053
Sunflo-H <sunflo0524@gmail.com>
2025-06-25 23:47:25 +0900

    필터 오버레이 작동방식 변경

--- src/FilterOverlay.tsx
+++ src/FilterOverlay.tsx
@@ -8,52 +8,50 @@ const FilterOverlay: React.FC<FilterOverlayProps> = ({
   toggleFilter,
 }) => {
   return (
-    <div
-      className={`filter-overlay bg-slate-200 border-b ${
-        isActive ? "active" : ""
-      }`}
-      id="filterOverlay"
-    >
-      <div>상품 필터</div>
-      <div className="filter-content">
-        <h2>성별</h2>
-        <label>
-          <input type="checkbox" /> 공용
-        </label>
-        <label>
-          <input type="checkbox" /> 남성
-        </label>
-        <label>
-          <input type="checkbox" /> 여성
-        </label>
-        <label>
-          <input type="checkbox" /> 키즈
-        </label>
+    <div className="filter-overlay-container">
+      <div className={`filter-blackbox ${isActive ? "active" : ""}`}></div>
+      <div className={`filter-overlay ${isActive ? "active" : ""}`}>
+        <div>상품 필터</div>
+        <div className="filter-content">
+          <h2>성별</h2>
+          <label>
+            <input type="checkbox" /> 공용
+          </label>
+          <label>
+            <input type="checkbox" /> 남성
+          </label>
+          <label>
+            <input type="checkbox" /> 여성
+          </label>
+          <label>
+            <input type="checkbox" /> 키즈
+          </label>
+        </div>
+        <div className="filter-content">
+          <h2>상품 종류</h2>
+          <label>
+            <input type="checkbox" /> 상의
+          </label>
+          <label>
+            <input type="checkbox" /> 하의
+          </label>
+          <label>
+            <input type="checkbox" /> 자켓
+          </label>
+          <label>
+            <input type="checkbox" /> 셔츠
+          </label>
+          <label>
+            <input type="checkbox" /> 셔츠
+          </label>
+          <label>
+            <input type="checkbox" /> 셔츠
+          </label>
+        </div>
+        <button id="closeBtn" onClick={toggleFilter}>
+          닫기
+        </button>
       </div>
-      <div className="filter-content">
-        <h2>상품 종류</h2>
-        <label>
-          <input type="checkbox" /> 상의
-        </label>
-        <label>
-          <input type="checkbox" /> 하의
-        </label>
-        <label>
-          <input type="checkbox" /> 자켓
-        </label>
-        <label>
-          <input type="checkbox" /> 셔츠
-        </label>
-        <label>
-          <input type="checkbox" /> 셔츠
-        </label>
-        <label>
-          <input type="checkbox" /> 셔츠
-        </label>
-      </div>
-      <button id="closeBtn" onClick={toggleFilter}>
-        닫기
-      </button>
     </div>
   );
 };
