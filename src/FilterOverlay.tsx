fdfe131c2e53d8e52e47646b83e0d8b3d259879c
Sunflo-H <sunflo0524@gmail.com>
2025-06-27 02:32:07 +0900

    필터 적용, 초기화 버튼 기능 및 디자인 적용

--- src/FilterOverlay.tsx
+++ src/FilterOverlay.tsx
@@ -32,6 +32,11 @@ const FilterOverlay: React.FC<FilterOverlayProps> = ({
     );
   };
 
+  const handleResetFilters = () => {
+    setSelectedGenders([]);
+    setSelectedCategories([]);
+  };
+
   return (
     <div className="filter-overlay-container max-w-[430px] w-full">
       <div className={`filter-blackbox ${isActive ? "active" : ""}`}></div>
@@ -46,7 +51,7 @@ const FilterOverlay: React.FC<FilterOverlayProps> = ({
           <div className="w-full text-left text-xl font-bold pt-4 pb-3">
             성별
           </div>
-          <div className="flex w-full gap-2 flex-wrap pb-6 border-b border-gray-200">
+          <div className="flex w-full gap-2 flex-wrap pb-8 border-b border-gray-200">
             {genderOptions.map((gender) => (
               <div
                 key={gender}
@@ -63,11 +68,11 @@ const FilterOverlay: React.FC<FilterOverlayProps> = ({
           </div>
         </div>
 
-        <div className="flex flex-col items-center px-4 mt-6">
+        <div className="flex flex-col items-center px-4 mt-2">
           <div className="w-full text-left text-xl font-bold pt-4 pb-3">
             상품 종류
           </div>
-          <div className="flex w-full gap-2 flex-wrap pb-6 border-b border-gray-200">
+          <div className="flex w-full gap-2 flex-wrap pb-6 ">
             {categoryOptions.map((category) => (
               <div
                 key={category}
@@ -87,6 +92,21 @@ const FilterOverlay: React.FC<FilterOverlayProps> = ({
                 {category}
               </div>
             ))}
+          </div>
+        </div>
+
+        <div className="flex justify-center items-center p-4 border-t border-gray-200">
+          <div
+            className="w-1/2 py-3 bg-white rounded border border-gray-300 text-gray-500 hover:border-blue-600 hover:text-blue-500 transition-colors duration-200 cursor-pointer mr-4"
+            onClick={handleResetFilters}
+          >
+            초기화
+          </div>
+          <div
+            className="w-1/2 py-3 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors duration-200 cursor-pointer"
+            onClick={toggleFilter}
+          >
+            적용
           </div>
         </div>
       </div>
