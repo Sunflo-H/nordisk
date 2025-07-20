fe15fe83877c25648b8cd71f6ca0d947f297af1f
Sunflo-H <sunflo0524@gmail.com>
2025-07-20 23:55:17 +0900

    Feat : filterOption 애플워치용 적용

--- src/components/Filter/FilterOption.tsx
+++ src/components/Filter/FilterOption.tsx
@@ -17,19 +17,23 @@ const FilterOption: React.FC<FilterOptionProps> = ({
     );
   };
   return (
-    <div className="flex flex-col items-center px-4 border-b border-gray-200">
-      <div className="filter-title w-full text-left text-xl font-bold pt-2 pb-1">
+    <div className="flex flex-col items-center px-4 border-b border-gray-200 pt-2 pb-3">
+      <div className="hidden aw:block w-full text-xl font-bold pb-1">
         {title}
       </div>
-      <div className="flex w-full gap-2 flex-wrap pb-3 ">
+      <div className="flex w-full gap-2 flex-wrap ">
         {options.map((option) => (
           <div
             key={option}
-            className={`cursor-pointer px-3 py-1 rounded-full border border-gray-300 hover:border-blue-500 transition-colors duration-200 ${
-              selectedOptions.includes(option)
-                ? "bg-blue-500 text-white"
-                : "bg-white text-gray-700"
-            }`}
+            className={` px-3 py-1 
+                rounded-full border border-gray-300 hover:border-blue-500 
+                transition-colors duration-200 
+                cursor-pointer
+                ${
+                  selectedOptions.includes(option)
+                    ? "bg-blue-500 text-white"
+                    : "bg-white text-gray-700"
+                }`}
             onClick={() => handleClick(option)}
           >
             {option}
